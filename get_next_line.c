5a1de2ed90e73f9888d9d2b605bc9afd9c465e12
Gabriel <gabrielpezoa26@gmail.com>
2024-11-20 15:59:59 -0300

    aaaaaa

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 15:35:49 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/11/20 15:29:44 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/11/20 15:58:12 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,38 +28,53 @@ static size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	return (ft_strlen(src));
 }
 
-static char	*fill_buffer(int fd, char *y)
+static char	*read_and_join(int fd, char *buffer, char *temp)
 {
 	ssize_t	bytes_read;
+	char	*new_buffer;
+
+	bytes_read = read(fd, temp, BUFFER_SIZE);
+	while (bytes_read > 0)
+	{
+		temp[bytes_read] = '\0';
+		new_buffer = ft_strjoin(buffer, temp);
+		free(buffer);
+		buffer = new_buffer;
+		bytes_read = read(fd, temp, BUFFER_SIZE);
+	}
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		buffer = NULL;
+	}
+	return (buffer);
+}
+
+static char	*fill_buffer(int fd, char *buffer)
+{
 	char	*temp;
-	char	*mango_loko;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	temp = malloc(sizeof(char) * (BUFFER_SIZE + 1));
 	if (temp == NULL)
 		return (NULL);
-	if (y == NULL)
-		y = ft_calloc(1, 1);
-	bytes_read = read(fd, temp, BUFFER_SIZE);
-	while (bytes_read > 0)
-	{
-		temp[bytes_read] = '\0';
-		mango_loko = ft_strjoin(y, temp);
-		free(y);
-		y = mango_loko;
-		bytes_read = read(fd, temp, BUFFER_SIZE);
-	}
+	if (buffer == NULL)
+		buffer = ft_calloc(1, 1);
+	buffer = read_and_join(fd, buffer, temp);
 	free(temp);
-	if (bytes_read == -1)
-		return (free(y), NULL);
-	return (y);
+	if (buffer && buffer[0] == '\0')
+	{
+		free(buffer);
+		buffer = NULL;
+	}
+	return (buffer);
 }
 
 static char	*line_extractor(char **buffer)
 {
 	int		index;
-	char	*xuxu;
+	char	*line;
 	char	*leftover;
 
 	if (*buffer == NULL || **buffer == '\0')
@@ -68,27 +83,31 @@ static char	*line_extractor(char **buffer)
 	while ((*buffer)[index] != '\0' && (*buffer)[index] != '\n')
 		index++;
 	if ((*buffer)[index] == '\n')
-		xuxu = malloc(sizeof(char) * (index + 2));
+		line = malloc(sizeof(char) * (index + 2));
 	else
-		xuxu = malloc(sizeof(char) * (index + 1));
-	if (xuxu == NULL)
+		line = malloc(sizeof(char) * (index + 1));
+	if (line == NULL)
 		return (NULL);
-	ft_strlcpy(xuxu, *buffer, index + 2);
+	ft_strlcpy(line, *buffer, index + 2);
 	if ((*buffer)[index] == '\n')
 		leftover = ft_strdup(&(*buffer)[index + 1]);
 	else
 		leftover = NULL;
 	free(*buffer);
 	*buffer = leftover;
-	return (xuxu);
+	return (line);
 }
 
 char	*get_next_line(int fd)
 {
 	static char	*buffer;
+	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buffer = fill_buffer(fd, buffer);
-	return (line_extractor(&buffer));
+	if (buffer == NULL)
+		return (NULL);
+	line = line_extractor(&buffer);
+	return (line);
 }
